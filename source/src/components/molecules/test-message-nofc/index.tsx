e25ea32f4d314776aee844c6fb043593b1ebcd37
Ismael Restrepo Vélez <irestrepo@celerik.com>
2022-04-21 14:14:53 -0500

    feat: another way of passing props

---
+++ source/src/components/molecules/test-message-nofc/index.tsx
@@ -0,0 +1,38 @@
+import React, { useMemo } from 'react';
+import { Box, Grid, Typography } from '@material-ui/core';
+
+interface Props {
+  darkTheme: boolean;
+}
+
+function TestComponent({ darkTheme }) {
+  function getText(): string {
+    if (darkTheme) {
+      return 'tema oscuro';
+    } else {
+      return 'tema claro';
+    }
+  }
+
+  return useMemo(
+    () => (
+      <Grid>
+        <Typography component='span'>
+          {getText()}
+          <Box
+            component='p'
+            fontWeight='fontWeightBold'
+            display='inline'
+            color={darkTheme ? '#FFF' : '#000'}
+          >
+            la guerrilla es real
+          </Box>
+          , me tienen secuestrado en la fantasia
+        </Typography>
+      </Grid>
+    ),
+    [darkTheme]
+  );
+}
+
+export default TestComponent;
